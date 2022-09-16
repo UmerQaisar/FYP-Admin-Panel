5f8d6b8f09f750d37fb539476760fbaf6ec60dd0
Umer <umerqaisar786@gmail.com>
2022-09-16 16:27:05 +0500

    Changed Credential File

--- app/controllers/concerns/initialize_firestore.rb
+++ app/controllers/concerns/initialize_firestore.rb
@@ -7,7 +7,7 @@ module InitializeFirestore
   extend ActiveSupport::Concern
 
   def create_firestore
-    @firestore = Google::Cloud::Firestore.new project_id: 'login-app-dc785', keyfile: ENV['GOOGLE_KEY_FILE']
+    @firestore = Google::Cloud::Firestore.new project_id: 'login-app-dc785', keyfile: ENV['GOOGLE_APPLICATION_CREDENTIALS']
     @users_ref = @firestore.col('Users')
   end
 end

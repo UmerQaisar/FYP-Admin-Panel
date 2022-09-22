aca9de174a0ec5f3448beddac3bb8a3ee3729b50
Umer <umerqaisar786@gmail.com>
2022-09-22 17:04:06 +0500

    Added Categories and Resolved JS calling Error

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,9 +1,20 @@
 class ReportsController < ApplicationController
   before_action :create_firestore
+  before_action :get_user
 
   def index
-    @user = @users_ref.doc(params[:patient_id]).get
     @reports = @users_ref.doc(params[:patient_id]).col("Reports").get
     @count = 0
+  end
+
+  def category
+    @reports = @users_ref.doc(params[:patient_id]).col("Reports").where("category", "=", params[:category]).get
+    @count = 0
+  end
+
+  private
+  def get_user
+    @user = @users_ref.doc(params[:patient_id]).get
+    @categories = @users_ref.doc(params[:patient_id]).col("Categories").get
   end
 end
